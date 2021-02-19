2af0eb5767119ae4824d87ce03f869cc057c02a9
ponnuse1437 <79296627+ponnuse1437@users.noreply.github.com>
2021-02-19 09:37:45 +0530

    Add files via upload

--- oddLetterCount.java
+++ oddLetterCount.java
@@ -1,1 +1,30 @@
+import java.util.scanner;
 
+public class oddLetterCount {
+
+	public static void mail (String args[]) {
+		
+		String str
+		int n = 0;
+		int m =0;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Input: ");
+		str = in.nextLine();
+		n = str.length()/2;
+		m = str.length()/2;
+		for (int i=0; i<= str.length(); i++) {
+			for (int j=0; j<= str.length();j++) {
+				system.out.println(str.charAt(n));
+				n++;
+				if(n = str.length()) {
+					n=0;
+				}	
+			}
+			m++;
+			if(m = str.length()) {
+				m=0;
+			}
+			
+		}
+	}
+}
